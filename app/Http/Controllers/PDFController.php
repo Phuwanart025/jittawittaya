3f478a79a3fa97044782859314753e94e277847a
Pure416100 <pakawat30807.6@gmail.com>
2023-01-21 13:44:55 +0700

    21/1/2066

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -1,33 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
-  
-use Illuminate\Http\Request;
-use Barryvdh\DomPDF\Facade\PDF;
-use Illuminate\Support\Facades\DB;
+
 use App\Models\rounds_completed;
+use Barryvdh\DomPDF\Facade\PDF;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PDFController extends Controller
 {
- 
-    public function pdf(Request $request,$id)
+
+    public function pdf(Request $request, $id)
     {
-        set_time_limit(0);  
+        set_time_limit(0);
         $data = DB::table('rounds_completed')->select('rounds_completed.*')->where('id', $id)->get();
         $pdf = PDF::loadView('myPDF', compact('data'));
+        $pdf->render();
         return $pdf->stream('รายงานการทำแบบบันทึก-jitdee.pdf');
+
     }
-    
+
     public function pdf2(Request $request)
     {
         $data = rounds_completed::all();
-        view()->share ('data', $data);
-        
-       
+        view()->share('data', $data);
+
         $pdf = PDF::loadView('myPDF', compact('data'));
         return $pdf->stream('รายงานการทำแบบบันทึก-jitdee.pdf');
-      
-    }  
-    
-   
+
+    }
+
 }
