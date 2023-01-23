803a643067ecb88b08d60d322f381887aa049347
Pure416100 <pakawat30807.6@gmail.com>
2023-01-23 15:22:47 +0700

    23/1/2066

--- app/Http/Controllers/diary_dayController.php
+++ app/Http/Controllers/diary_dayController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\diary_day2;
 use App\Models\diary_day;
 use App\Models\rounds_completed;
 use Illuminate\Http\Request;
@@ -15,27 +16,26 @@ class diary_dayController extends Controller
             $this->validate($request, [
                 'flexRadioDefault' => 'required',
                 'value' => 'required',
-                'v3' => 'required',
+                'v3' => 'required'
             ]);
-            $latest = rounds_completed::where('user_id', Auth::user()->id)->orWhere('jobs_id', 1)->orderBy('id', 'desc')->first();
+            $latest = rounds_completed::where('user_id', Auth::user()->id)->where('jobs_id', 1)->orderBy('id', 'desc')->first();
             $diary1 = new rounds_completed;
             $diary = new diary_day;
-            if ($latest) {
-                $diary1->user_id = Auth::user()->id;
-                $diary1->jobs_id = 2;
-                if (is_null($latest)) {
-                    $diary1->rounds = 1;
-                } else {
-                    $diary1->rounds = $latest->rounds + 1;
-                }
-                $diary->color_feel_today = $request->flexRadioDefault;
-                $diary->sensation_level = $request->input('value');
-                $diary->detail_t1 = $request->v3;
-                $diary1->save();
-                $diary->diary_day()->associate($diary1);
-                $diary->save();
+            if(empty($latest) || is_null($latest)){
+                $diary1->rounds = 1;
+            } else {
+                $diary1->rounds = $latest->rounds + 1;
             }
-        } catch (\Exception$e) {
+            $diary1->user_id = Auth::user()->id;
+            $diary1->jobs_id = 1;
+            $diary1->score = 10;
+            $diary->color_feel_today = $request->flexRadioDefault;
+            $diary->sensation_level = $request->input('value');
+            $diary->detail_t1 = $request->v3;
+            $diary1->save();
+            $diary->diary_day()->associate($diary1);
+            $diary->save();
+        } catch (\Exception $e) {
             return redirect()->back()->withErrors(['Error' => $e->getMessage()]);
         }
         return redirect('/results');
@@ -47,28 +47,26 @@ class diary_dayController extends Controller
             $this->validate($request, [
                 'flexRadioDefault' => 'required',
                 'value' => 'required',
-                'v3' => 'required',
+                'v3' => 'required'
             ]);
-            $latest = rounds_completed::where('user_id', Auth::user()->id)->orWhere('jobs_id', 2)->orderBy('id', 'desc')->first();
+            $latest = rounds_completed::where('user_id', Auth::user()->id)->where('jobs_id', 2)->orderBy('id', 'desc')->first();
             $diary1 = new rounds_completed;
             $diary = new diary_day;
-            if ($latest) {
-                $diary1->user_id = Auth::user()->id;
-                $diary1->jobs_id = 2;
-                if (is_null($latest)) {
-                    $diary1->rounds = 1;
-                } else {
-                    $diary1->rounds = $latest->rounds + 1;
-                }
-                $diary1->score = 10;
-                $diary->color_feel_today = $request->flexRadioDefault;
-                $diary->sensation_level = $request->input('value');
-                $diary->detail_t1 = $request->v3;
-                $diary1->save();
-                $diary->diary_day()->associate($diary1);
-                $diary->save();
+            if(empty($latest) || is_null($latest)){
+                $diary1->rounds = 1;
+            } else {
+                $diary1->rounds = $latest->rounds + 1;
             }
-        } catch (\Exception$e) {
+            $diary1->user_id = Auth::user()->id;
+            $diary1->jobs_id = 2;
+            $diary1->score = 10;
+            $diary->color_feel_today = $request->flexRadioDefault;
+            $diary->sensation_level = $request->input('value');
+            $diary->detail_t1 = $request->v3;
+            $diary1->save();
+            $diary->diary_day()->associate($diary1);
+            $diary->save();
+        } catch (\Exception $e) {
             return redirect()->back()->withErrors(['Error' => $e->getMessage()]);
         }
         return redirect('/results');
