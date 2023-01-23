83543a3e0b8a8b42370ee6a60f1919ede554f312
Pure416100 <pakawat30807.6@gmail.com>
2023-01-23 14:42:36 +0700

    23/1/2066

--- app/Http/Controllers/diary_dayController.php
+++ app/Http/Controllers/diary_dayController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\diary_day2;
 use App\Models\diary_day;
 use App\Models\rounds_completed;
 use Illuminate\Http\Request;
@@ -11,57 +10,68 @@ use Illuminate\Support\Facades\Auth;
 class diary_dayController extends Controller
 {
     public function store(Request $request)
-{
-    try {
-        $this->validate($request, [
-            'flexRadioDefault' => 'required',
-            'value' => 'required',
-            'v3' => 'required'
-        ]);
-        $latest = rounds_completed::where('user_id')->orderBy('jobs_id', 'desc')->first();
-        $diary1 = new rounds_completed;
-        $diary = new diary_day;
-        if ($latest) {
-            $diary1->user_id = Auth::user()->id;
-            $diary1->jobs_id = 1;
-            if(rounds_completed::count() === 0 ){
-                $diary1->rounds = 1;
-            } else {
+    {
+        try {
+            $this->validate($request, [
+                'flexRadioDefault' => 'required',
+                'value' => 'required',
+                'v3' => 'required',
+            ]);
+            $latest = rounds_completed::where('user_id', Auth::user()->id)->orWhere('jobs_id', 1)->orderBy('id', 'desc')->first();
+            $diary1 = new rounds_completed;
+            $diary = new diary_day;
+            if ($latest) {
+                $diary1->user_id = Auth::user()->id;
+                $diary1->jobs_id = 2;
+                if (is_null($latest)) {
+                    $diary1->rounds = 1;
+                } else {
                     $diary1->rounds = $latest->rounds + 1;
                 }
-            
-            $diary1->score = 10;
-            $diary->color_feel_today = $request->flexRadioDefault;
-            $diary->sensation_level = $request->input('value');
-            $diary->detail_t1 = $request->v3;
-            $diary1->save();
-            $diary->diary_day()->associate($diary1);
-            $diary->save();
+                $diary->color_feel_today = $request->flexRadioDefault;
+                $diary->sensation_level = $request->input('value');
+                $diary->detail_t1 = $request->v3;
+                $diary1->save();
+                $diary->diary_day()->associate($diary1);
+                $diary->save();
+            }
+        } catch (\Exception$e) {
+            return redirect()->back()->withErrors(['Error' => $e->getMessage()]);
         }
-    } catch (\Exception $e) {
-        return redirect()->back()->withErrors(['Error' => $e->getMessage()]);
+        return redirect('/results');
     }
-    return redirect('/results');
-}
 
     public function store2(Request $request)
     {
-        $latest = rounds_completed::where('user_id')->orderBy('jobs_id', 'desc')->first();
-        $diary1 = new rounds_completed;
-        $diary1->user_id = Auth::user()->id;
-        $diary1->jobs_id = 2;
-        $diary1->rounds = $latest->rounds + 1;
-        $diary1->score = 10;
-        $diary1->save();
-
-        $diary = new diary_day2();
-        $diary->color_feel_today = $request->flexRadioDefault;
-        $diary->sensation_level = $request->input('value');
-        $diary->detail_t1 = $request->v3;
-        $diary->diary_day()->associate($diary1);
-        $diary->save();
-
+        try {
+            $this->validate($request, [
+                'flexRadioDefault' => 'required',
+                'value' => 'required',
+                'v3' => 'required',
+            ]);
+            $latest = rounds_completed::where('user_id', Auth::user()->id)->orWhere('jobs_id', 2)->orderBy('id', 'desc')->first();
+            $diary1 = new rounds_completed;
+            $diary = new diary_day;
+            if ($latest) {
+                $diary1->user_id = Auth::user()->id;
+                $diary1->jobs_id = 2;
+                if (is_null($latest)) {
+                    $diary1->rounds = 1;
+                } else {
+                    $diary1->rounds = $latest->rounds + 1;
+                }
+                $diary1->score = 10;
+                $diary->color_feel_today = $request->flexRadioDefault;
+                $diary->sensation_level = $request->input('value');
+                $diary->detail_t1 = $request->v3;
+                $diary1->save();
+                $diary->diary_day()->associate($diary1);
+                $diary->save();
+            }
+        } catch (\Exception$e) {
+            return redirect()->back()->withErrors(['Error' => $e->getMessage()]);
+        }
         return redirect('/results');
-
     }
+
 }
