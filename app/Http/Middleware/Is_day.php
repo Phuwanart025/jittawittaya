44411e8308bdcd53afdbd17053d28bec7127eb14
Pure416100 <pakawat30807.6@gmail.com>
2023-03-17 17:10:02 +0700

    16/3/2066 555555555555555

--- app/Http/Middleware/Is_day.php
+++ app/Http/Middleware/Is_day.php
@@ -10,6 +10,7 @@ use App\Models\rounds_completed;
 use App\Models\jobs_21day;
 use Carbon\Carbon;
 use RealRashid\SweetAlert\Facades\Alert;
+
 class Is_day
 {
     /**
@@ -21,46 +22,48 @@ class Is_day
      */
     public function handle(Request $request, Closure $next)
     {
-        // หา jobs_id ลำดับสูงสุดของผู้ใช้งาน
+        // Find the highest jobs_id of the user
         $last_round_completed = rounds_completed::where('user_id', Auth::user()->id)
             ->join('jobs_21day', 'rounds_completed.jobs_id', '=', 'jobs_21day.id')
             ->select('rounds_completed.jobs_id', DB::raw('COUNT(rounds_completed.id) as completed_rounds'), 'rounds_completed.created_at')
             ->groupBy('rounds_completed.jobs_id', 'rounds_completed.created_at')
             ->orderByDesc('jobs_id')
             ->first();
-    
+
         if (!$last_round_completed || $last_round_completed->completed_rounds === 21) {
             if ($request->route()->getName() !== 'day1') {
                 return redirect()->route('day1');
             }
-        } else {
-            $last_completed_time = Carbon::parse($last_round_completed->created_at);
-            $current_time = Carbon::now();
-            $elapsed_time = $current_time->diffInHours($last_completed_time);
-    
-            if ($elapsed_time && $elapsed_time < 12) {
-                $end_time = $last_completed_time->addHours(12)->format('d-m-Y H:i:s');
-                $message = 'คุณสามารถเข้าสู่รอบถัดไปได้หลังจาก ' . $end_time;
-                $next_job = $last_round_completed->jobs_id + 1;
-    
-                if ($next_job) {
-                    $message .= ' วันถัดไป: วันที่ ' . $next_job;
-                }
-    
-                Alert::error($message, 'error')->timerProgressBar()->persistent(true)->autoClose(false);
-                return redirect()->back();
+        
+    } else {
+        $last_completed_time = Carbon::parse($last_round_completed->created_at);
+        $current_time = Carbon::now();
+        $elapsed_time = $current_time->diffInHours($last_completed_time);
+
+        // If the last job was completed less than 12 hours ago, show an alert message
+        if ($elapsed_time < 12) {
+            $end_time = $last_completed_time->addHours(12)->format('d-m-Y H:i:s');
+            $message = 'คุณสามารถเข้าสู่รอบถัดไปได้หลังจาก ' . $end_time;
+            $next_job = $last_round_completed->jobs_id + 1;
+
+            if ($next_job) {
+                $message .= ' วันถัดไป: วันที่ ' . $next_job;
             }
-        }
-    
+
+            Alert::error($message, 'error')->timerProgressBar()->persistent(true)->autoClose(false);
+            return redirect()->back();
+        }    
+        
+        // If it has been 12 hours or more since the last job was completed, proceed to the next job
         if ($last_round_completed && $last_round_completed->jobs_id < 21) {
             $next_round = $last_round_completed->jobs_id + 1;
-    
-            // ตรวจสอบว่าเส้นทาง URL ใหม่ต่างกับเดิมหรือไม่
+
+            // Check if the new URL is different from the old one
             if ($request->route()->getName() !== 'day'.$next_round) {
                 return redirect()->route('day'.$next_round);
             }
         }
-    
+ }
         return $next($request);
     }
 }
